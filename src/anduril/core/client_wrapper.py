1028308d1c722b995368378b4d7ccc5924355e99
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2026-03-25 09:23:28 -0400

    SDK Generation

--- src/anduril/core/client_wrapper.py
+++ src/anduril/core/client_wrapper.py
@@ -27,12 +27,12 @@ class BaseClientWrapper:
         import platform
 
         headers: typing.Dict[str, str] = {
-            "User-Agent": "anduril-lattice-sdk/4.3.2",
+            "User-Agent": "anduril-lattice-sdk/4.4.1",
             "X-Fern-Language": "Python",
             "X-Fern-Runtime": f"python/{platform.python_version()}",
             "X-Fern-Platform": f"{platform.system().lower()}/{platform.release()}",
             "X-Fern-SDK-Name": "anduril-lattice-sdk",
-            "X-Fern-SDK-Version": "4.3.2",
+            "X-Fern-SDK-Version": "4.4.1",
             **(self.get_custom_headers() or {}),
         }
         token = self._get_token()
